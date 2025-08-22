d1d6212d333c0210ecee485dd64ed029892797b0
StaZarycki <stazarycki@gmail.com>
2025-08-22 14:04:37 +0200

    initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+import {defineConfig} from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: {js, eslintPluginAstro},
+        extends: ["js/recommended"],
+        languageOptions: {globals: globals.browser}
+    },
+    tseslint.configs.recommended,
+]);
